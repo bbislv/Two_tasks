021a945c76416c00f83df6c45018d87f39352147
bbislv <bbislv12@gmail.com>
2026-06-24 16:31:36 +0300

    Create Task2.java

---
+++ Task2.java
@@ -0,0 +1,15 @@
+import java.util.Arrays;
+
+public class Task2 {
+    public static void main(String[] args) {
+        int[] array = {1, 2, 3, 4, 5};
+
+        for (int i = 0; i < array.length / 2; i++) {
+            int temp = array[i];
+            array[i] = array[array.length - 1 - i];
+            array[array.length - 1 - i] = temp;
+        }
+
+        System.out.println(Arrays.toString(array));
+    }
+}
